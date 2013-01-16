ed2e7d3978c522ff5c60c55577dfc59d113374da
Ryan Barrett <git@ryanb.org>
2013-01-15 16:06:36 -0800

    implemented twitter oauth

--- main.py
+++ main.py
@@ -5,19 +5,22 @@ feed, ie tweets from people you follow.
 Based on both plusstreamfeed and salmon-unofficial.
 """
 
+
 __author__ = 'Ryan Barrett <twitter-atom@ryanb.org>'
 
 import json
 import logging
 import os
 import urllib
-import urlparse
+from webob import exc
 
 import appengine_config
 from activitystreams import twitter
 from activitystreams.webutil import util
 from activitystreams.webutil import webapp2
+import tweepy
 
+from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 
 
@@ -26,83 +29,73 @@ GENERATED_TEMPLATE_FILE = os.path.join(os.path.dirname(__file__),
 ATOM_TEMPLATE_FILE = os.path.join(os.path.dirname(__file__),
                                   'activitystreams', 'templates', 'user_feed.atom')
 
-API_HOME_URL = (
-  'https://graph.twitter.com/me?access_token=%s&fields='
-  'home,id,username,name,link,updated_time,bio,location')
-API_HOME_COUNT = 25  # default number of posts returned
-
 # based on salmon-unofficial/twitter.py.
-# twitter api url templates. can't (easily) use urllib.urlencode() because i
-# want to keep the %(...)s placeholders as is and fill them in later in code.
-# TODO: use appengine_config.py for local mocktwitter vs prod twitter
-GET_AUTH_CODE_URL = '&'.join((
-    'https://www.twitter.com/dialog/oauth/?scope=read_stream,offline_access',
-    'client_id=%(client_id)s',
-    # redirect_uri here must be the same in the access token request!
-    'redirect_uri=%(host_url)s/got_auth_code',
-    'response_type=code',
-    'state=%(state)s',
-    ))
-
-GET_ACCESS_TOKEN_URL = '&'.join((
-    'https://graph.twitter.com/oauth/access_token?client_id=%(client_id)s',
-    # redirect_uri here must be the same in the oauth request!
-    # (the value here doesn't actually matter since it's requested server side.)
-    'redirect_uri=%(host_url)s/got_auth_code',
-    'client_secret=%(client_secret)s',
-    'code=%(auth_code)s',
-    ))
+OAUTH_CALLBACK = '%s://%s/oauth_callback' % (appengine_config.SCHEME,
+                                             appengine_config.HOST)
 
 
-class GenerateHandler(webapp2.RequestHandler):
-  """Registers the current user and generates a feed URL for their stream.
+class OAuthToken(db.Model):
+  """Datastore model class for an OAuth token.
+  """
+  token_key = db.StringProperty(required=True)
+  token_secret = db.StringProperty(required=True)
 
-  Based on AddTwitter in salmon-unofficial/twitter.py.
+
+class StartAuthHandler(webapp2.RequestHandler):
+  """Starts three-legged OAuth with Twitter.
+
+  Fetches an OAuth request token, then redirects to Twitter's auth page to
+  request an access token.
+  """
+  def get(self):
+    try:
+      auth = tweepy.OAuthHandler(appengine_config.TWITTER_APP_KEY,
+                                 appengine_config.TWITTER_APP_SECRET,
+                                 OAUTH_CALLBACK)
+      auth_url = auth.get_authorization_url()
+    except tweepy.TweepError, e:
+      msg = 'Could not create Twitter OAuth request token: '
+      logging.exception(msg)
+      raise exc.HTTPInternalServerError(msg + `e`)
+
+    # store the request token for later use in the callback handler
+    OAuthToken(token_key = auth.request_token.key,
+               token_secret = auth.request_token.secret,
+               ).put()
+    logging.info('Generated request token, redirecting to Twitter: %s', auth_url)
+    self.redirect(auth_url)
+
+
+class CallbackHandler(webapp2.RequestHandler):
+  """The OAuth callback. Fetches an access token and redirects to the front page.
   """
 
-  def post(self):
-    """Starts generating a feed URL by requesting a Twitter auth code.
-
-    After retrieving an auth code, redirects to /twitter_got_auth_code,
-    which makes the next request to get the access token.
-    """
-    logging.info('Generating a new feed. Asking FB for auth code.')
-
-    url = GET_AUTH_CODE_URL % {
-      'client_id': appengine_config.TWITTER_APP_ID,
-      # TODO: CSRF protection identifier.
-      # http://developers.twitter.com/docs/authentication/
-      'host_url': self.request.host_url,
-      'state': self.request.host_url + '/got_auth_token',
-      }
-    self.redirect(url)
-
-
-class GotAuthCode(webapp2.RequestHandler):
   def get(self):
-    """Gets an access token based on an auth code."""
-    auth_code = self.request.get('code')
-    assert auth_code
-    logging.info('got auth code: %s', auth_code)
+    oauth_token = self.request.get('oauth_token', None)
+    oauth_verifier = self.request.get('oauth_verifier', None)
+    if oauth_token is None:
+      raise exc.HTTPBadRequest('Missing required query parameter oauth_token.')
 
-    redirect_uri = urllib.unquote(self.request.get('state'))
-    assert '?' not in redirect_uri
+    # Lookup the request token
+    request_token = OAuthToken.gql('WHERE token_key=:key', key=oauth_token).get()
+    if request_token is None:
+      raise exc.HTTPBadRequest('Invalid oauth_token: %s' % oauth_token)
 
-    # TODO: handle permission declines, errors, etc
-    url = GET_ACCESS_TOKEN_URL % {
-      'auth_code': auth_code,
-      'client_id': appengine_config.TWITTER_APP_ID,
-      'client_secret': appengine_config.TWITTER_APP_SECRET,
-      'host_url': self.request.host_url,
-      }
-    logging.info('getting access token via %s', url)
-    resp = util.urlfetch(url)
-    # TODO: error handling. handle permission declines, errors, etc
-    logging.info('access token response: %s' % resp)
-    params = urlparse.parse_qs(resp)
-    access_token = params['access_token'][0]
+    # Rebuild the auth handler
+    auth = tweepy.OAuthHandler(appengine_config.TWITTER_APP_KEY,
+                               appengine_config.TWITTER_APP_SECRET)
+    auth.set_request_token(request_token.token_key, request_token.token_secret)
 
-    atom_url = '%s/atom?access_token=%s' % (self.request.host_url, access_token)
+    # Fetch the access token
+    try:
+      access_token = auth.get_access_token(oauth_verifier)
+    except tweepy.TweepError, e:
+      msg = 'Twitter OAuth error, could not get access token: '
+      logging.exception(msg)
+      raise exc.HTTPInternalServerError(msg + `e`)
+
+    atom_url = '%s/atom?access_token_key=%s&access_token_secret=%s' % (
+      self.request.host_url, access_token.key, access_token.secret)
     logging.info('generated feed URL: %s', atom_url)
     self.response.out.write(template.render(GENERATED_TEMPLATE_FILE,
                                             {'atom_url': atom_url}))
@@ -118,10 +111,10 @@ class AtomHandler(webapp2.RequestHandler):
     assert access_token
     resp = json.loads(util.urlfetch(API_HOME_URL % access_token))
 
-    fb = twitter.Twitter(self)
-    actor = fb.user_to_actor(resp)
+    tw = twitter.Twitter(self)
+    actor = tw.user_to_actor(resp)
     posts = resp.get('home', {}).get('data', [])
-    activities = [fb.post_to_activity(p) for p in posts]
+    activities = [tw.post_to_activity(p) for p in posts]
 
     self.response.headers['Content-Type'] = 'text/xml'
     self.response.out.write(template.render(
@@ -135,8 +128,8 @@ class AtomHandler(webapp2.RequestHandler):
 
 
 application = webapp2.WSGIApplication(
-  [('/generate', GenerateHandler),
-   ('/got_auth_code', GotAuthCode),
+  [('/generate', StartAuthHandler),
+   ('/oauth_callback', CallbackHandler),
    ('/atom', AtomHandler),
    ],
   debug=appengine_config.DEBUG)
