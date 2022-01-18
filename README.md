f4947505e0b836de941a9e190950583750640598
Ryan Barrett <git@ryanb.org>
2022-01-17 22:45:09 -0800

    switch from app_server to flask-gae-static

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ tweets from people you follow.
 Deployed on App Engine at http://twitter-atom.appspot.com/
 
 License: This project is placed in the public domain.
+
+
+Development
+---
+1. Fork and clone this repo.
+1. `pip install -r requirements.txt`, optionally in a virtualenv.
+1. Install the [Google Cloud SDK](https://cloud.google.com/sdk/) with the `gcloud-appengine-python` and `gcloud-appengine-python-extras` [components](https://cloud.google.com/sdk/docs/components#additional_components).
+1. `GAE_ENV=localdev FLASK_ENV=development FLASK_APP=main.py flask run -p 8080`
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from urllib.parse import urlencode
 from flask import Flask, render_template, request
 from flask.views import View
 from flask_caching import Cache
+import flask_gae_static
 from granary import atom, microformats2, twitter
 from oauth_dropins import twitter as oauth_twitter
 from oauth_dropins.webutil import appengine_config, appengine_info, flask_util, util
@@ -27,6 +28,7 @@ app.config.from_mapping(
 )
 app.after_request(flask_util.default_modern_headers)
 app.register_error_handler(Exception, flask_util.handle_exception)
+flask_gae_static.init_app(app)
 app.wsgi_app = flask_util.ndb_context_middleware(
     app.wsgi_app, client=appengine_config.ndb_client)
 
